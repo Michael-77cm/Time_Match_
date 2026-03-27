9bde4fe444bb97ce9ee6db46b84e935e66d3d6f4
Michael-77cm <michaelbello175@gmail.com>
2026-03-27 01:11:58 +0000

    scheduler/admin.py file updated

--- timematch/scheduler/admin.py
+++ timematch/scheduler/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Availability, Event, Membership, Notification
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+	list_display = ("title", "code", "created_by", "finalized_datetime", "created_at")
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+	list_display = ("event", "user", "joined_at")
+
+@admin.register(Availability)
+class AvailabilityAdmin(admin.ModelAdmin):
+	list_display = ("event", "user", "date", "start_time", "end_time", "status")
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+	list_display = ("user", "event", "message", "is_read", "created_at")
