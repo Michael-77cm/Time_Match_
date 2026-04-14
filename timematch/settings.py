a20eeba9400a5c47d5f0a57c2e320b3351f7938f
Michael-77cm <michaelbello175@gmail.com>
2026-04-14 21:06:29 +0100

    ff

--- timematch/settings.py
+++ timematch/settings.py
@@ -138,11 +138,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 LOGIN_REDIRECT_URL = 'create_event'
 LOGOUT_REDIRECT_URL = 'home'
 CSRF_TRUSTED_ORIGINS = [
